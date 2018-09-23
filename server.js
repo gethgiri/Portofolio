3021a303bbe6faf595f0a5c38660bd02a15cbfee
AdhiJordan <adhithyaprabhu77@gmail.com>
2018-09-23 13:33:25 +0530

    backe nd change

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-require('babel-core/register')({
-  "presets": ["es2015", "react", "stage-1"]
-})
-
 "use strict"
 var express = require('express');
 var app = express();
@@ -10,19 +6,6 @@ var fs = require('fs');
 var client = require('./client.js');
 var Transmit = require('react-transmit');
 
- function handleRender(req, res) {
- 	console.log("fucntion called");
-  Transmit.renderToString(client).then(({reactString, reactData}) => {
-  	console.log("reactString", reactString);
-    fs.readFile('./public/index.html', 'utf8', function (err, data) {
-      if (err) throw err;
-
-const document = data.replace(/<div id="container"><\/div>/, `<div id="container"><section role="main" class="react-container"><div>${reactString}</div></section></div>`);
-      const output = Transmit.injectIntoMarkup(document, reactData, ['/build/client.js']);
-      res.send(output);
-    });
-  });
-}
 
 
 // Serve built files with static files middleware
