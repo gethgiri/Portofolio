2e62fdec1219d1ae5eb0ea285e6535301feb4d05
AdhiJordan <adhithyaprabhu77@gmail.com>
2018-09-23 12:26:44 +0530

    added new UI changes with some features in it

--- client/Components/HomePage/HomePage.js
+++ client/Components/HomePage/HomePage.js
@@ -25,6 +25,11 @@ class HomePage extends Component{
 	    });
  	}
 
+ 	goTop(){
+	 	document.body.scrollTop = 0;
+	    document.documentElement.scrollTop = 0;
+ 	}
+
  	showNavigationSelection(){
  		this.setState({
  			showNav: !this.state.showNav
@@ -40,11 +45,16 @@ class HomePage extends Component{
 		const showNavCls = (this.state.showNav) ? "show-nav" : "";
  		return (
 			<div>
-				<div className="portfolio-header-page">
-
-				 
-					<label className="w3-display-middle" style={{fontSize:"52px"}}> <hr/>  UI Developer & designer  <hr/> </label>
-				
+				<div className="portfolio-header-page ">
+				<div className="w3-row">
+					<div className="w3-col s3 m3 l3">&nbsp;</div>
+					<div className="w3-col s6 m6 l6">
+						<p  className="w3-center" style={{fontSize:"52px"}}><hr /> Software Developer  </p>
+						<p  className="w3-center" style={{fontSize:"52px"}}> &   </p>
+						<p  className="w3-center" style={{fontSize:"52px"}}>UX-Designer <hr /></p>
+					</div>
+					<div className="w3-col s3 m3 l3">&nbsp;</div>
+				</div>
                </div>
 
 				<div className={"portfolio-navigation w3-bar " + addCls}>
@@ -52,191 +62,165 @@ class HomePage extends Component{
 				 	<button className="w3-button w3-teal w3-large tile-icon"
 				 	onClick={this.showNavigationSelection.bind(this)}>☰</button>
 				  	<div className={"header-right " + showNavCls}>
-						<a>HOME </a>
-						<a>ABOUT ME </a>
-						<a>SKILLS </a>
-						<a>RESUME </a>
-						<a>CONTACT US </a>
+						<a>PORTFOLIO </a>
 					</div>
 				</div>
 
+
 				<div>
-					<section style={{backgroundColor:"#2424"}}>
-						<div className="">
-						
-							<div className="w3-col s12 m12 l6">
-								<h1 style={{marginLeft:"100px"}}>GOALS</h1>
-								<p className="w3-left" style={{fontSize:"16px",marginLeft:"120px"}}>
-									My dream or goal whatever. i would like to create a full application
-									from A to z. from 0 to every thing own idea designing, frontend,
-									Backend, hosting.i believe experience and learning give everything.
-						   	    </p>
-							</div>
-							<div className="w3-col s12 m12 l6">
-								<h1 style={{marginLeft:"100px"}} >HOBBIES</h1>
-								
-								<img style={{width:"450px",height:"200px",marginLeft:"120px"}} src="./assets/ring.png" alt="logo" /><br/><br/>
-
-								<label style={{marginLeft:"140px"}}>
-									 Sports is my only HOBBIES
-						   	    </label>
-							</div>
+				<div id="goals-section">
+					<section className="goals-section" 
+					style={{backgroundColor: "rgb(255, 255, 255)" }}>
+					<div className="w3-row">
+						<div className="w3-col s12 m12 l6">
+							<h1 style={{marginLeft: "100px"}}>GOALS</h1>
+							<p className="w3-left" style={{fontSize: "24px", marginLeft: "120px"}}>
+								My dream or goal whatever is to create a full application from 
+								A to z. From 0 to every thing like designing, frontend, 
+								Backend, hosting. And i also interested in cyber-security.
+							 	I believe experience and very enthusiastic to learn new technologies.
+							</p>
 						</div>
-					</section>
-					<section className="about-section w3-container w3-row" style={{backgroundColor:"#2424"}}>
-						
-			                <div className="w3-container w3-padding-16">
-					   	    	<div className="w3-row">
-					   	    		<div className="">
-					   	    			
-					   	    			<h1>My Work</h1>
-					   	    			<label>
-											Here I add some of my work during the learings.
-								   	    </label> <br/>
-								   	   <div className="w3-row">
-								   	       <div className="w3-col l6 m12 s12">
-								   	        		<img src="./assets/Screen 1.png" style={{width:"600px",padding:"10px"}}/>
-								   	       </div>
-
-								   	        <div className="w3-col l6 m12 s12">
-								   	        		<img src="./assets/Screen 3.png" style={{width:"600px",padding:"10px"}}/>
-								   	        </div>
-								   	   </div>
-
-								   	    <div className="w3-row">
-								   	       <div className="w3-col l6 m12 s12">
-								   	        		<img src="./assets/Screen 2.png" style={{width:"600px",padding:"10px"}}/>
-								   	       </div>
-
-								   	        <div className="w3-col l6 m12 s12">
-								   	        		<img src="./assets/Screen 4.png" style={{width:"600px",padding:"10px"}}/>
-								   	        </div>
-								   	   </div>
-
-								   	   </div>
-					   	    		</div>
-					   	    		
-					   	    	</div>
-					   	    	
-					   	  
-						
-					</section>
-					<section className="skillset-section" style={{backgroundColor:"#dbecd5"}}>
-						<div className="w3-container w3-row">
-						    <div className="background2">
-							<div className="w3-col s12 m12 l6 w3-left w3-padding-16">
-								<h1 className="skills-highlight">SKILLS</h1>
-								<label>
-									Here i mentioned some of my knowledge level of particular skills. I put it on my own knowledge 
-						   	    </label>
-							</div>
-							<div className="w3-col s12 m12 l6">
-								<label>Illustrator</label><br />
-								<div className="w3-light-grey">
-								  <div className="w3-container w3-green w3-center" style={{width: "80%"}}>80%</div>
-								</div><br />
-								<label>CSS</label><br />
-								<div className="w3-light-grey">
-								  <div className="w3-container w3-green w3-center" style={{width: "75%"}}>75%</div>
-								</div><br />
-								<label>React js</label><br />
-								<div className="w3-light-grey">
-								  <div className="w3-container w3-green w3-center" style={{width: "80%"}}>80%</div>
-								</div><br />
-								<label>JavaScript</label><br />
-								<div className="w3-light-grey">
-								  <div className="w3-container w3-green w3-center" style={{width: "80%"}}>80%</div>
-								</div>
-							</div>
-						  </div>
+						<div className="w3-col s12 m12 l6">
+							 <h1 style={{marginLeft: "100px"}}>HOBBIES</h1>
+							 <img src="./assets/ring.png" alt="logo" style={{marginLeft: "120px", maxWidth: "300px"}} />
+							 <br /><br />
+							 <label style={{marginLeft: "140px"}}>Sports is my only HOBBIES</label>
 						</div>
+					</div>
 					</section>
-					<section className="resume-section w3-center " style={{background: "#f2f2f1"}}>
-						
-							<div className="w3-content w3-display-container resume-arrow">
-							<h1 className="resume-highlight">RESUME</h1>
-							<label>
-								For further more details about me .i attached my resume with it.
-								please kindly have a look at this. may be i am the person you are looking for. 
-						   </label><br /><br />
-						   <a href="upload/Resume.pdf" download="Resume.pdf">
-						   <button className="portfolio-button-primary w3-button w3-center w3-hover-none"
-						   	style={{border: "1px solid green", color: "green"}}>
-						   	DOWNLOAD RESUME
-							</button>
-							</a>
-						</div>
-					</section>
-					<section className="work-experience-section">
-						<div className="w3-container">
-							<h1 className="work-highlight w3-left">WORK EXPERIENCE</h1>
-							<br />
-							<div className="w3-row">
-								<div className="w3-col s12 m12 l6 w3-padding-16">
-								<label>
-										I Started my carrer as a technical guy
-										i have much more intrested in hardwares.
-										so I decided to choses this field. later 
-										i started focussing on all the domains. 
-								</label>
-								</div>
-								<div className="w3-col s12 m12 l6 w3-row">
-									<div className="w3-card w3-col s12 m12 l12 w3-padding-16 w3-container"
-									style={{marginBottom: "22px"}}>
-										<h1 className="w3-center company-highlight">Airlet softwares</h1>
-										
-										<br />
-										<label>Position:   ATE (Tech Engg)</label><br/>
-										<label>Position:   Part time (UI Designer)</label>
-									</div>
-								</div>
-							</div>
-						</div>
-					</section>
-					
-					<section className="contact-section">
-						<div className="parallax w3-row w3-container">
-							<div className="w3-col s12 m12 l6 w3-padding-16 w3-left">
-								<h1 className="contact-highlight w3-center" style={{color: "#fff"}}>CONTACT ME</h1>
-								<div className="">
-								<div className="contact-information">
-									<label style={{color: "#919191"}}>
-										Email : saravanan.r2394@gmail.com <br />
-										Mobile & Whats app : 9487789913<br />
-									</label>
-								</div>
-								</div>
-							</div>
-							<div className="w3-col s12 m12 l6 w3-padding-16">
-								<form>
-									<div className="w3-row">
-										<label style={{color: "#fff"}}>Name  </label><mark>*</mark><br />
-										<input type="text" placeholder="Name here...." className="contact-fields" />
-									</div><br />
-									<div className="w3-row">
-										<label style={{color: "#fff"}}>E-Mail  </label><mark>*</mark><br />
-										<input type="text" placeholder="E-Mail here...." className="contact-fields" />
-									</div><br />
-									<div className="w3-row">
-										<label style={{color: "#fff"}}>Message  </label><mark>*</mark><br />
-										<textarea className="contact-fields" rows="10" />
-									</div>
-									<div className="w3-row" style={{paddingTop: "20px"}}>
-									   <button className="portfolio-button-primary w3-button w3-left w3-hover-none"
-									   	style={{background: "#191f21", color: "white", border: "none"}}>
-									   	CONTACT ME
-										</button>
-									</div>
-								</form>
-							</div>
-						</div>
-					</section>
-					<footer className="footer-section">
-						<label className="w3-display-middle">&copy; 2018 Saravanan r</label>
-					</footer>
 				</div>
+				 <div id="skillset-section">
+				 	<section className="skillset-section" 
+				 		style={{backgroundColor: "rgb(219, 236, 213)" }}>
+				 		<div className="w3-container w3-row">
+				 		<div className="background2">
+				 		<div className="w3-col s12 m12 l6 w3-left w3-padding-16">
+				 			<h1 className="skills-highlight">SKILLS</h1>
+					 		<label style={{fontSize: "24px"}}>
+					 		Here i mentioned some of my knowledge level 
+					 		of particular skills. I put it on my own knowledge
+					 		</label>
+				 		</div>
+
+				 		<div className="w3-col s12 m12 l6">
+
+				 		<label>C</label><br />
+				 		<div className="w3-light-grey">
+				 			<div className="w3-container w3-green w3-center" style={{width: "80%"}}>80%</div>
+				 		</div><br />
+
+				 		<label>HTML &amp; CSS</label><br />
+				 		<div className="w3-light-grey">
+				 			<div className="w3-container w3-green w3-center" style={{width: "75%"}}>75%</div>
+				 		</div><br />
+
+				 		<label>Python</label><br />
+				 		<div className="w3-light-grey">
+				 			<div className="w3-container w3-green w3-center" style={{width: "30%"}}>30%</div>
+				 		</div><br />
+
+				 		<label>Photoshop</label><br />
+				 		<div className="w3-light-grey">
+				 			<div className="w3-container w3-green w3-center" style={{width: "80%"}}>80%</div>
+				 		</div><br />
+
+				 		<label>JavaScript</label><br />
+				 		<div className="w3-light-grey">
+				 			<div className="w3-container w3-green w3-center" style={{width: "80%"}}>80%</div>
+				 		</div>
+				 		<br />
+
+				 		<label>React js</label><br />
+				 		<div className="w3-light-grey">
+				 			<div className="w3-container w3-green w3-center" style={{width: "80%"}}>30%</div>
+				 		</div>
+
+				 		</div>
+				 		</div>
+				 		</div>
+				 		</section>
+				 		</div>
 
 
+				 		<div id="resume-section">
+					 		<section className="resume-section w3-center" 
+					 		style={{background: "rgb(242, 242, 241)"}}>
+					 		<div className="w3-content w3-display-container resume-arrow">
+						 		<h1 className="resume-highlight">RESUME</h1>
+						 		<label style={{fontSize: "24px"}}>For further more details about me .
+						 		i attached my resume with it. please kindly 
+						 		have a look at this. may be i am the person 
+						 		you are looking for.</label>
+						 		<br /><br />
+						 		<a href="upload/Resume.doc" download="Resume.doc">
+						 		<button className="portfolio-button-primary w3-button w3-center w3-hover-none"
+						 		 style={{border: "1px solid green", color: "green" }}>
+						 		 DOWNLOAD RESUME</button>
+						 		</a><br /> <br/>
+						 		<h1 className="resume-highlight">CURICULLUM VITAE</h1>
+						 		<label style={{fontSize: "24px"}}>For further more details about me .
+						 		i attached my curicullum vitae with it. please kindly 
+						 		have a look at this. may be i am the person 
+						 		you are looking for.</label>
+						 		<br /><br />
+						 		<a href="upload/curicullumVitae.doc" download="curicullumVitae.doc">
+						 		<button className="portfolio-button-primary w3-button w3-center w3-hover-none"
+						 		 style={{border: "1px solid green", color: "green" }}>
+						 		 DOWNLOAD CURICULLUM VITAE</button>
+						 		</a>
+					 		 </div>
+					 		 </section>
+				 		 </div>
+
+
+				 		 <div id="about-section">
+					 		 <section className="about-section w3-container w3-row" 
+					 		 style={{backgroundColor: "rgba(34, 68, 34, 0.267)"}}>
+					 		 <div className="w3-container w3-padding-16">
+						 		 <div className="w3-row"><div>
+						 		 <h1 className="w3-center">Some of my work sample!</h1>
+						 		 <br />
+						 		 <div className="w3-row">
+							 		 <div className="w3-col  m12 l6 w3-center">
+							 			<img src="./assets/giriwork1.png" className="detectOne" />
+							 		 </div>
+							 		 <div className="w3-col  m12 l6 w3-center">
+							 		 	<img src="./assets/giriwork2.png" className="detectTwo" />
+							 		 </div>
+						 		 </div>
+						 		 </div>
+						 		 </div>
+					 		 </div>
+				 		 </section>
+				 		 </div>
+
+				 		<div id="contact-section">
+					 		<section className="contact-section w3-center">
+						 		<div className="parallax w3-row w3-container">
+						 		<div className="w3-col s12 m12 l12 w3-padding-16 w3-left">
+						 		<h1 className="contact-highlight w3-center" 
+						 		style={{color: "rgb(255, 255, 255)"}}>CONTACT ME</h1>
+						 		<div className="">
+						 		<div className="contact-information">
+							 		<h3 style={{color: "rgb(145, 145, 145)" }}>
+							 		Email : giriharan777@gmail.com <br />Mobile &amp; Whats app : 7358873120<br />
+							 		Linked In: https://www.linkedin.com/in/giri-402a99134 <br /> Github: https://github.com/gethgiri<br />
+							 		</h3>
+						 		</div>
+						 		</div>
+						 		</div>
+						 		</div>
+					 		</section>
+				 		</div>
+				 		<footer className="footer-section">
+				 		<label className="w3-display-middle">© 2018 giriharan D</label>
+				 		<br /><br /><br /><br />
+				 		<button className="w3-button w3-black w3-hover-cyan"
+				 		onClick={this.goTop.bind(this)}>
+				 		Go To Top
+				 		</button>
+				 		</footer></div>
 			</div>
 		);
 	}
